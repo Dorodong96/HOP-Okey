31e226ef8b70782b92c6e7addb5e1d117ff767c7
Shin-jun <inside1451@naver.com>
2022-09-20 23:15:57 +0900

    [ADD] 프로젝트 시작
    프로젝트 시작

---
+++ OkeyBot/OkeyBot/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OkeyBot
+//
+//  Created by Shin yongjun on 2022/09/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OkeyBot/OkeyBot/OkeyBotApp.swift
@@ -0,0 +1,17 @@
+//
+//  OkeyBotApp.swift
+//  OkeyBot
+//
+//  Created by Shin yongjun on 2022/09/20.
+//
+
+import SwiftUI
+
+@main
+struct OkeyBotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
